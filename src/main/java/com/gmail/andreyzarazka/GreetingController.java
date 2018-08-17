734a86ed1083420e5085dfd268633b90d53e0268
Andrew <andreyzarazka@gmail.com>
2018-08-17 17:04:10 +0300

    Added dependencies for working with the database. Created the entity of the "Message", implemented a repository to work with this entity.

--- src/main/java/com/gmail/andreyzarazka/GreetingController.java
+++ src/main/java/com/gmail/andreyzarazka/GreetingController.java
@@ -1,13 +1,24 @@
 package com.gmail.andreyzarazka;
 
+import com.gmail.andreyzarazka.domain.Message;
+import com.gmail.andreyzarazka.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public GreetingController(final MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
@@ -17,7 +28,32 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hi, Jack");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+
         return "main";
     }
 }
---
+++ src/main/java/com/gmail/andreyzarazka/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.gmail.andreyzarazka.repos;
+
+import com.gmail.andreyzarazka.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
